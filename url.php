b5940b5c16eb2fd1f2982f4dfaf548c7a1397f70
vovichua <vovichua@gmail.com>
2018-11-17 11:46:15 +0200

    вывел массив з тела цикла

--- url.php
+++ url.php
@@ -6,15 +6,47 @@
  * Time: 16:15
  */
 
-$url = explode(PHP_EOL, $_POST['this']);
+$urls = explode(PHP_EOL, $_POST['this']);
 
-foreach ($url as $value) {
-    $item = array('http' , 'https');
-    $search = array('https', 'http');
+$invalidUrls = [];
+foreach ($urls as $url) {
+    $url = trim($url);
+    $url = rtrim($url, '/');
+    $parsedParams = parse_url($url);
 
-    $res = str_replace($search,$item,$value);
-    echo "<pre>";
-    var_dump($res);
+    $protocol = $parsedParams['scheme'];
+    if ($protocol == 'http') {
+        $replaceTo = 'https';
+        if (!isset($parsedParams['path'])) {
+            $modifiedUrl .= '/index.php';
+        }
+    } elseif ($protocol == 'https') {
+        $replaceTo = 'http';
+    } else {
+        $invalidUls[] =  $url;
+        continue;
+    }
+
+
+
+    $modifiedUrl = str_replace($protocol, $replaceTo, $url);
+
+    $newUrl[] = $modifiedUrl;
 }
+//echo "<pre>";
+//var_dump($newUrl);
 
+foreach ($newUrl as $new) {
 
+    $ch = curl_init($ch);
+
+    curl_setopt($ch, CURLOPT_URL, $new);
+
+    curl_exec($ch);
+    if (!curl_errno($ch)) {
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        echo 'Код ответа: '.$httpCode."<br><br>";
+
+    }
+    curl_close($ch);
+}
